e787b0654ec35c6c1cd1579400071ccecb204036
stackgik <stackgik@gmail.com>
2024-10-08 19:24:12 +0100

    change the QuestionTile styles

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks(.*)` route is not protected here
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/api/webhooks/clerk',
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
